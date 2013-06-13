851aad6952a5dcd02eb5a346838c3043bea1de2a
Trystan Spangler <trystan.s@comcast.net>
2013-06-12 22:32:11 -0700

    plants spread on each tick

--- src/Plant.scala
+++ src/Plant.scala
@@ -2,15 +2,21 @@ import scala.util.Random
 import scala.swing.Color
 import scala.swing.Graphics2D
 
-class Plant(x: Int, y: Int, color: Color) {
+class Plant(var x: Int, var y: Int, color: Color) {
 
+  var age = 0
+  
   def this(rng: Random) = this(
     rng.nextInt(200),
     rng.nextInt(200),
-    new Color(128 + rng.nextInt(64), 128 + rng.nextInt(64), 128 + rng.nextInt(64)))
-
-  def update = {
-    
+    new Color(128 + rng.nextInt(64), 128 + rng.nextInt(64), 128 + rng.nextInt(64))
+  )
+  
+  def alive = { age < 10 }
+  
+  def update(w: World) = {
+    age += 1
+    w.addSeed(new Plant(x, y, color))
   }
   
   def draw(g: Graphics2D): Unit = {
--- src/World.scala
+++ src/World.scala
@@ -6,17 +6,38 @@ class World(width: Int, height: Int) {
   var rng = new Random();
   var climate = new Climate(width, height, rng)
   
+  var plantGrid = Array.ofDim[Plant](width, height)
   var plantList = Vector.empty :+ new Plant(rng)
   for (_ <- 0 to (width * height / 100))
-    plantList = plantList :+ new Plant(rng)
+	  addSeed(new Plant(rng))
 
   def update = {
     climate.update
-    plantList.foreach(p => p.update)
+    plantList.foreach(p => p.update(this))
+    removeDeadPlants
   }
   
   def draw(g: Graphics2D) = {
     climate.draw(g)
     plantList.foreach(p => p.draw(g))
+  }
+  
+  def addSeed(plant: Plant): Unit = {
+    plant.x += rng.nextInt(6) - 3
+    plant.y += rng.nextInt(6) - 3
+    
+    if (plant.x < 0 || plant.x >= width || plant.y < 0 || plant.y >= height)
+      return
+    
+    if (plantGrid(plant.x)(plant.y) != null)
+      return
+    
+    plantGrid(plant.x)(plant.y) = plant
+    plantList = plantList :+ plant
+  }
+  
+  def removeDeadPlants = {
+    plantList.filter(p => !p.alive).foreach(p => plantGrid(p.x)(p.y) = null)
+    plantList = plantList.filter(p => p.alive)
   }
 }
