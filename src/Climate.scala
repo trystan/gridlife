d279a37cc779ae03262171d1574d7c1ecf4a21ea
Trystan Spangler <trystan.s@comcast.net>
2013-06-13 19:36:47 -0700

    draw recent climate

--- src/Climate.scala
+++ src/Climate.scala
@@ -6,15 +6,21 @@ class Climate(width: Int, height: Int, rng: Random) {
   
   var grid = Array.ofDim[Int](width, height)
   
+  def shortCycle = Math.sin(Math.Pi * 2 * time * 10 / 11) * 0.5
+  def mediumCycle = Math.sin(Math.Pi * 2 * time * 10 / 197) * 0.75
+  def longCycle = Math.sin(Math.Pi * 2 * time * 10 / 499)
+  def cycles = shortCycle + mediumCycle + longCycle
+  
   def climateAt(x: Int, y: Int):Int = {
-    return grid(x)(y)
+    return Math.max(0, Math.min(grid(x)(y), 8))
   }
   
   def update = {
-    time += 0.0001
+    time += 0.001
     
     for (x <- 0 until width)
     for (y <- 0 until height)
-      grid(x)(y) = ((ImprovedNoise.noise(x * 0.033, y * 0.033, time) + 1.0) * 4.5).toInt
+      grid(x)(y) = ((ImprovedNoise.noise(x * 0.033, y * 0.033, time) + 1.0) * 4.5 
+    		  		+ cycles).toInt 
   }
 }
--- src/Main.scala
+++ src/Main.scala
@@ -6,7 +6,7 @@ object gridlife extends SimpleSwingApplication {
 
   var world = new World(200, 150)
   world.update
-  
+
   def top = new MainFrame {
     title = "Grid Life"
     size = new Dimension(800, 700)
@@ -16,35 +16,54 @@ object gridlife extends SimpleSwingApplication {
     val locationLabel = new Label { text = " No plant selected" }
     val colorLabel = new Label
     val dnaLabel = new Label
-    
+
+    var recentClimate = Vector.empty[Double]
+    val climateSampleRate = 10
+    var climateSampleCounter = 0
+
     contents = new BoxPanel(Orientation.Vertical) {
-      { 
-    	  contents += new Panel {
-		    size = new Dimension(800, 600)
-		    preferredSize = new Dimension(800, 600)
-		    minimumSize = new Dimension(800, 600)
-	        override def paintComponent(g: Graphics2D) = {
-	          super.paintComponent(g)
-	          
-	          world.view.draw(g)
-	        }
-		    listenTo(mouse.clicks)
-			reactions += {
-			  case e: MouseClicked =>
-			    select(world.plantAt(e.point.x / 4, e.point.y / 4))
-			}
-      	  }
-    	  contents += new BoxPanel(Orientation.Vertical) {
-		    size = new Dimension(800, 100)
-		    preferredSize = new Dimension(800, 100)
-		    minimumSize = new Dimension(800, 100)
-		    contents += locationLabel
-		    contents += colorLabel
-		    contents += dnaLabel
-    	  }
+      {
+        contents += new Panel {
+          size = new Dimension(800, 600)
+          preferredSize = new Dimension(800, 600)
+          minimumSize = new Dimension(800, 600)
+          override def paintComponent(g: Graphics2D) = {
+            super.paintComponent(g)
+
+            world.view.draw(g)
+          }
+          listenTo(mouse.clicks)
+          reactions += {
+            case e: MouseClicked =>
+              select(world.plantAt(e.point.x / 4, e.point.y / 4))
+          }
+        }
+        contents += new BoxPanel(Orientation.Horizontal) {
+          contents += new Panel {
+            size = new Dimension(200, 100)
+            preferredSize = new Dimension(200, 100)
+            minimumSize = new Dimension(200, 100)
+            override def paintComponent(g: Graphics2D) = {
+              super.paintComponent(g)
+              g.drawString("Climate", 2, 12)
+              for (t <- 0 until 200) {
+                if (recentClimate.length > t + 1)
+                  g.drawLine(t, (recentClimate(t) * -15 + 50).toInt, t + 1, (recentClimate(t + 1) * -15 + 50).toInt)
+              }
+            }
+          }
+          contents += new BoxPanel(Orientation.Vertical) {
+            size = new Dimension(600, 100)
+            preferredSize = new Dimension(600, 100)
+            minimumSize = new Dimension(600, 100)
+            contents += locationLabel
+            contents += colorLabel
+            contents += dnaLabel
+          }
+        }
       }
     }
-  
+
     def select(plant: Plant) = {
       if (plant == null) {
         locationLabel.text = "No plant selected"
@@ -53,21 +72,29 @@ object gridlife extends SimpleSwingApplication {
       } else {
         locationLabel.text = "Location: " + plant.x + "," + plant.y + "\t\tClimate: " + (world.climateAt(plant.x, plant.y) + 1)
         colorLabel.text = "Color: " + plant.color.getRed() + " " + plant.color.getGreen() + " " + plant.color.getBlue()
-    	dnaLabel.text = "Eergy gained per climate: "
-    	for (i <- 0 until plant.energyPerClimate.length)
-    	  dnaLabel.text += " " + (i+1) + ":" + plant.energyPerClimate(i)
+        dnaLabel.text = "Energy gained per climate: "
+        for (i <- 0 until plant.energyPerClimate.length)
+          dnaLabel.text += " " + (i + 1) + ":" + plant.energyPerClimate(i)
       }
     }
-    
+
     def update = {
       world.update
+      climateSampleCounter += 1
+
+      if (climateSampleCounter == climateSampleRate) {
+        climateSampleCounter = 0
+        recentClimate = recentClimate :+ world.climate.cycles
+        if (recentClimate.length > 200)
+          recentClimate = recentClimate.tail
+      }
       this.repaint
     }
-    
-    val timer = new javax.swing.Timer((1.0 / 30.0).toInt, 
-        new javax.swing.AbstractAction() {
-	    	def actionPerformed(e : java.awt.event.ActionEvent) = update
-  	    })
+
+    val timer = new javax.swing.Timer((1.0 / 30.0).toInt,
+      new javax.swing.AbstractAction() {
+        def actionPerformed(e: java.awt.event.ActionEvent) = update
+      })
     timer.setRepeats(true)
     timer.start()
   }
